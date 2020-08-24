d9a94d5776b68bfba21bc185c719ca3b82982bbb
si-hyun-park <psih8032@gmail.com>
2020-08-24 20:33:27 +0900

    python_study

---
+++ practice.py
@@ -0,0 +1,32 @@
+from math import*
+from random import*
+
+print(abs(-5))  # 절대값
+print(pow(4, 3))  # 4^3
+print(max(5, 12))  # 12
+print(min(5, 12))  # 5
+print(round(3.14))  # 3
+print(round(4.99))  # 5
+print("======")
+print(floor(4.99))
+print(ceil(3.14))
+print(sqrt(16))
+print("-=--")
+# print(random())  # 0.0 ~ 1.0 의 임의의 값을 생성
+# print(random()*10)
+# print(int(random()*10)+1)
+# print(int(random()*10)+1)
+# print(int(random()*10)+1)
+# print(int(random()*10)+1)
+# print(int(random()*10)+1)
+
+print(int(random()*45)+1)
+print(int(random()*45)+1)
+print(int(random()*45)+1)
+print(int(random()*45)+1)
+print(int(random()*45)+1)
+print(int(random()*45)+1)
+
+print(randrange(1, 46))
+print("-----------")
+print(randint(1, 45))
